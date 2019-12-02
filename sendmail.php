472960adeaffc0eaf34b51ca401de1274826c9a7
hakkem pm <hakkempalakkal@gmail.com>
2019-12-03 00:54:33 +0530

    test

---
+++ sendmail.php
@@ -0,0 +1,40 @@
+	<html>
+        <head></head>
+        <body>
+        <!-- use PHPMailer\PHPMailer\PHPMailer;
+            <?php 
+				// 	if(isset($_POST["submit"])){
+                //         $eol = PHP_EOL;
+                //     $to = "sanashukkur123@gmail.com";
+				// 	$from=$_POST["email"];
+				// 	$name=$_POST["name"];
+				// 	$subject=$_POST["subject"];
+				// 	$msg=$_POST["message"];
+				// 	$headers = 'From:'. $from .$eol; 
+				// if(	mail($to,$name,$subject,$msg,$headers))
+                //     echo '<p style="color: #dd2b1c;margin:10px 0 0 0;">	mail send successfully</p>';
+                //     else
+                //     echo '<p style="color: #dd2b1c;margin:10px 0 0 0;">	mail not send..try again</p>';
+                // 	}
+             
+  
+               
+               
+                // require 'PHPMailerAutoload.php';
+                // $mail = new PHPMailer;
+                // $mail->setFrom('sanashukkur123@gmail.com', 'sana');
+                // $mail->addAddress('sanashukkur123@gmail.com', 'My Friend');
+                // $mail->Subject  = 'First PHPMailer Message';
+                // $mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+                // if(!$mail->send()) {
+                //   echo 'Message was not sent.';
+                //   echo 'Mailer error: ' . $mail->ErrorInfo;
+                // } else {
+                //   echo 'Message has been sent.';
+                // }
+               
+	// ?>
+
+
+</body>
+</html>
